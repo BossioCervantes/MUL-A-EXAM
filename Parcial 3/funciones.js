a1927d7b6ee1b8ba619c7d9b64caca71cf9aca2b
BossioCervantes <124269531+BossioCervantes@users.noreply.github.com>
2023-05-22 22:54:30 -0500

    Update funciones.js

--- Parcial 3/funciones.js
+++ Parcial 3/funciones.js
@@ -1,100 +1,77 @@
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerHeight;
+  // Configuración del tamaño y color de fondo del renderizador
+  var WIDTH = window.innerWidth;
+  var HEIGHT = window.innerHeight;
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(WIDTH, HEIGHT);
-renderer.setClearColor(0xDDDDDD, 1);
-document.body.appendChild(renderer.domElement);
-var scene = new THREE.Scene();
+  var renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.setClearColor(0xDDDDDD, 1);
+  document.body.appendChild(renderer.domElement);
+  var scene = new THREE.Scene();
 
-// Variables para la Grilla y Flechas
-var size = 20;
-var arrowSize = 5;
-var divisions = 20;
-var origin = new THREE.Vector3( 0, 0, 0 );
-var x = new THREE.Vector3( 1, 0, 0 );
-var y = new THREE.Vector3( 0, 1, 0 );
-var z = new THREE.Vector3( 0, 0, 1 );
-var color2 = new THREE.Color( 0x333333 ); 
-var colorR = new THREE.Color( 0xAA0000 );
-var colorG = new THREE.Color( 0x00AA00 );
-var colorB = new THREE.Color( 0x0000AA );
+  // Variables para la Grilla y Flechas
+  var size = 20;
+  var arrowSize = 5;
+  var divisions = 20;
+  var origin = new THREE.Vector3( 0, 0, 0 );
+  var x = new THREE.Vector3( 1, 0, 0 );
+  var y = new THREE.Vector3( 0, 1, 0 );
+  var z = new THREE.Vector3( 0, 0, 1 );
+  var color2 = new THREE.Color( 0x333333 ); 
+  var colorR = new THREE.Color( 0xAA0000 );
+  var colorG = new THREE.Color( 0x00AA00 );
+  var colorB = new THREE.Color( 0x0000AA );
 
-//Crear la Grilla
-var gridHelperXZ = new THREE.GridHelper( size, divisions, color2, color2);
+  // Crear la Grilla
+  var gridHelperXZ = new THREE.GridHelper( size, divisions, color2, color2);
 
-//Flechas
-var arrowX = new THREE.ArrowHelper( x, origin, arrowSize, colorR );
-var arrowY = new THREE.ArrowHelper( y, origin, arrowSize, colorG );
-var arrowZ = new THREE.ArrowHelper( z, origin, arrowSize, colorB );
+  // Flechas
+  var arrowX = new THREE.ArrowHelper( x, origin, arrowSize, colorR );
+  var arrowY = new THREE.ArrowHelper( y, origin, arrowSize, colorG );
+  var arrowZ = new THREE.ArrowHelper( z, origin, arrowSize, colorB );
 
-// Posición de la camara
-var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT);
-camera.position.x = 0;
-camera.position.y = 2;
-camera.position.z = 10;
-const light = new THREE.AmbientLight(0x404040, 5);
+  // Posición de la cámara
+  var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT);
+  camera.position.x = 0;
+  camera.position.y = 2;
+  camera.position.z = 10;
 
-// Se añaden los elementos a la escena
-scene.add(light);
-scene.add(camera);
-scene.add(gridHelperXZ);
-scene.add(arrowX);
-scene.add(arrowY);
-scene.add(arrowZ);
+  const light = new THREE.AmbientLight(0x404040, 5);
 
-// Funciones
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+  directionalLight.position.set(1, 1, 1);
+  scene.add(directionalLight);
 
-function crearPoligono(nlados, radio) {
-    const geometry = new THREE.BufferGeometry();
-    const vertices = [];
-  
-    // Se calculan de los vértices del polígono
-    const ang = (2 * Math.PI) / nlados;
-    for (let i = 0; i < nlados + 1; i++) {
-      const x = radio * Math.cos(i * ang);
-      const y = radio * Math.sin(i * ang);
-      vertices.push(x, y, 0); // Se agregan la coordenada Z como 0
-    }
-  
-    // Se agregan los vértices a la geometría
-    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-  
-    // Se crea un material para el polígono
-    const material = new THREE.LineBasicMaterial({ color: 0x000fff });
-  
-    // Se crea un objeto de malla que combina la geometría y el material
-    const poligono = new THREE.Line(geometry, material);
-  
-    return poligono;
-  }
+  // Se añaden los elementos a la escena
+  scene.add(light);
+  scene.add(camera);
+  scene.add(gridHelperXZ);
+  scene.add(arrowX);
+  scene.add(arrowY);
+  scene.add(arrowZ);
 
+  // Función para crear un polígono 3D
   function crearPoligono3D(nlados, radio, altura) {
-    const geometry = new THREE.BufferGeometry();
+    const geometry = new THREE.CylinderBufferGeometry();
     const vertices = [];
     const indices = [];
 
     // Se calculan los vértices del polígono
     const ang = (2 * Math.PI) / nlados;
-    for (let i = 0; i < nlados; i++) {
-        const x = radio * Math.cos(i * ang);
-        const y = radio * Math.sin(i * ang);
-        vertices.push(x, y, 0);
-        vertices.push(x, y, altura);
+    for (let i = 0; i < nlados + 1; i++) {
+      const x = radio * Math.cos(i * ang);
+      const y = radio * Math.sin(i * ang);
+      vertices.push(x, y, 0);
+      vertices.push(x / 3, y / 3, altura);
     }
-
-    // Se agrega los dos últimos vértices para cerrar las caras
-    vertices.push(radio, 0, 0);
-    vertices.push(radio, 0, altura);
 
     // Se asigna los vértices a la geometría
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
     // Se agrega los índices para definir las caras
     for (let i = 0; i < nlados; i++) {
-        const startIndex = i * 2;
-        indices.push(startIndex, startIndex + 1, startIndex + 3);
-        indices.push(startIndex, startIndex + 3, startIndex + 2);
+      const startIndex = i * 2;
+      indices.push(startIndex, startIndex + 1, startIndex + 3);
+      indices.push(startIndex, startIndex + 3, startIndex + 2);
     }
 
     // Añadimos los índices para cerrar las caras superior e inferior
@@ -102,53 +79,68 @@ function crearPoligono(nlados, radio) {
     const ultimoVertice = numVertices - 1;
     const penultimoVertice = numVertices - 2;
     for (let i = 0; i < nlados; i++) {
-        indices.push(ultimoVertice - i * 2, ultimoVertice - (i + 1) * 2, penultimoVertice);
-        indices.push(i * 2, (i + 1) * 2, ultimoVertice - i * 2 - 1);
+      indices.push(ultimoVertice - i * 2, ultimoVertice - (i + 1) * 2, penultimoVertice);
+      indices.push(i * 2, (i + 1) * 2, ultimoVertice - i * 2 - 1);
     }
 
     // Se asignan los índices a la geometría
     geometry.setIndex(indices);
 
-    // Se crea un material para el polígono
-    const material = new THREE.LineBasicMaterial({ color: 0x000fff });
+    // Se genera un color aleatorio
+    const randomColor = new THREE.Color(Math.random(), Math.random(), Math.random());
+
+    // Se crea un material con el color aleatorio
+    const material = new THREE.MeshBasicMaterial({ color: randomColor });
 
     // Se crea un objeto de malla que combina la geometría y el material
-    const poligono = new THREE.Line(geometry, material);
+    const poligono = new THREE.Mesh(geometry, material);
+    poligono.castShadow = true; // Habilitar lanzamiento de sombras
+    poligono.receiveShadow = true; // Habilitar recepción de sombras
 
     return poligono;
-}
+  }
 
+  // Función para crear los polígonos
+  function crearPoligonos() {
+    const cantidadPoligonos = 4;
+    const radio = 1;
+    const altura = 2;
+    const separacion = 3;
 
-var poligoninho = crearPoligono(5,1);
-//scene.add(poligoninho);
+    // Se crean los 4 polígonos inferiores
+    for (let i = 0; i < cantidadPoligonos; i++) {
+      const nlados = 5;
+      const poligono = crearPoligono3D(nlados, radio, altura);
 
-Piramide1 = crearPoligono3D(5,1,2);
-Piramide2 = crearPoligono3D(5,1,2);
-Piramide3 = crearPoligono3D(5,1,2);
-Piramide4 = crearPoligono3D(5,1,2);
+      const posicionX = (i - (cantidadPoligonos - 1) / 2) * separacion;
+      poligono.position.x = posicionX;
+      poligono.rotation.x = Math.PI/0.67;
+      scene.add(poligono);
+    }
 
-scene.add(Piramide1);
-scene.add(Piramide2);
-scene.add(Piramide3);
-scene.add(Piramide4);
+    // Se crean los 4 polígonos superiores
+    for (let i = 0; i < cantidadPoligonos; i++) {
+      const nlados = 5;
+      const poligono = crearPoligono3D(nlados, radio, altura);
 
-Piramide1.rotation.x = Math.PI/0.67
-Piramide2.rotation.x = Math.PI/0.67
-Piramide3.rotation.x = Math.PI/0.67
-Piramide4.rotation.x = Math.PI/0.67
+      const posicionX = (i - (cantidadPoligonos - 1) / 2) * separacion;
+      const posicionY = altura * 2; // Colocar encima de los primeros polígonos
+      poligono.position.set(posicionX, posicionY, 0);
+      poligono.rotation.x = Math.PI/0.67;
+      scene.add(poligono);
+    }
+  }
 
-Piramide1.position.x = 5
-Piramide2.position.x = 1.5
-Piramide3.position.x = -1.5
-Piramide4.position.x = -5
+  // Se llaman a las funciones para crear los polígonos y animar la escena
+  crearPoligonos();
 
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);  
 
-const controls = new THREE.OrbitControls(camera, renderer.domElement);  
-
-function animate() {
+  function animate() {
     requestAnimationFrame(animate);
     controls.update();
     renderer.render(scene, camera);
   }
-  animate();
 
+  animate();
+  renderer.shadowMap.enabled = true;
